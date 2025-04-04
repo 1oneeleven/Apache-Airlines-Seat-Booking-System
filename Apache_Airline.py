03c43578658752e77daef2287451fb7c1ea36c49
1oneeleven <lgnd_247@icloud.com>
2025-04-04 15:15:19 +0100

    Update Apache_Airline.py

--- Apache_Airline.py
+++ Apache_Airline.py
@@ -1,3 +1,8 @@
 # the following code is a commit test for apache airlines booking system
 
-print ("apache airlines")
+
+
+# The following code is the Apache airlines seat booking system for FC723 Project assessment 5
+
+print ("welcome to apache airlines!")
+
