349da298c04697cae3531c9398519323e531ca75
1oneeleven <lgnd_247@icloud.com>
2025-04-06 21:47:49 +0100

    fixed bugs and missed code

--- Apache_Airline.py
+++ Apache_Airline.py
@@ -9,20 +9,21 @@ class ApacheAirlines:
     
     def __init__(self):                         #Set seat map for the Burak 757 airplane
         self.seat_map = [
-    
             [" 1A  ", " 1B ", "  1C ", " |X| ", "  1D  ", " 1E ", "  1F "],
             [" 2A  ", " 2B ", "  2C ", " |X| ", "  2D  ", " 2E ", "  2F "],
             [" 3A  ", " 3B ", "  3C ", " |X| ", "  3D  ", " 3E ", "  3F "],
             [" 4A  ", " 4B ", "  4C ", " |X| ", "  4D  ", " 4E ", "  4F "],
             [" 5A  ", " 5B ", "  5C ", " |X| ", "  5D  ", " 5E ", "  5F "],
-                            
-            ]
+        ]
         
-        for row_num in range(10, 70):                    #Fill in remaining rows between row 10 to row 75
+        #Fill in remaining rows between row 10 to row 75
+        for row_num in range(10, 70):                    
             self.seat_map.append([
                 f" {row_num}A ", f" {row_num}B ", f" {row_num}C ", "|X| ", f" {row_num}D", f"  {row_num}E ", f" {row_num}F"
                 ])
-        for row_num in range(71, 75):                    #Fill in remaining rows between row 10 to row 75
+            
+        #Fill in remaining rows between row 10 to row 75
+        for row_num in range(71, 75):                    
             self.seat_map.append([
                 f"B-{row_num}A", f"B-{row_num}B", f"B-{row_num}C", "|X| ", f"B-{row_num}D", f"B-{row_num}E", f"B-{row_num}F"
                 ])
@@ -36,36 +37,31 @@ class ApacheAirlines:
     
 #_______________________________functions_______________________________
 
-
 #function to display seat map 
 def display_seats(seats):
-    for row in seats:           #for loop to loop through every seat
-        print(" ".join(row))    #print the seats in from the list in a row pattern
-
+    for row in seats:                   #for loop to loop through every seat
+        print(" ".join(row))            #print the seats in from the list in a row pattern
 
 #check if the seat is available
 def check_availability(seats, row, col):
-    return seats[row][col] == "F"
+    return not seats[row][col].strip() in ["R", " |X| ", "S", "S", "S"]
 
 #function to book seats
 def book_seat(seats, row, col):
-    
-    if check_availability(seats, row, col):             #checks if the seat is availble 
-        seats[row][col] = "R"
-        print("Seat booked successfully!")
+    if check_availability(seats, row, col):          
+        seats[row][col] = "  F  "                               #checks whether the seat is Reserved "R"
+        print("Seat booked successfully!")                  
     else:
-        print("Seat is already booked or unavailable.") #displays error if seat is unavailable
+        print("Seat is already booked or unavailable.")     #displays error if seat is Reserved "R"
 
 #function to free booked seat
 def free_seat(seats, row, col):
     
-    if seats[row][col] == "R":          
+    if seats[row][col].strip() == "R":          
         seats[row][col] = "F"           #reassignes the seat from "R" to "F"
         print("Seat freed successfully!")
     else:                               #displays that the seat is not 
         print("Seat is not booked.")
-
-
 
 #_______________________________main screen_______________________________
     
@@ -111,8 +107,8 @@ def main():
         #choice to exit the booking system
         elif choice == "5":
             print ("Exiting system... thank you for visiting Apache Airlines:)")
-            
             break
+        
         else:
             print ("Invalid choice. Please enter a number between 1-5.")
 
